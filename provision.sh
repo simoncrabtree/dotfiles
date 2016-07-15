169984617e14f0edd78239f198912fbfc521e4c8
Simon Crabtree <simon@crabsticks.net>
2016-07-15 10:17:10 +0100

    Added provision script to set up a linux dev environment (WIP)

---
+++ provision.sh
@@ -0,0 +1,52 @@
+#enable firewall
+sudo ufw enable
+
+sudo apt-get install software-properties-common
+sudo apt-get install curl
+
+#neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get -y install neovim
+sudo pip install neovim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+
+#nodejs
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo export PATH=~/.npm-global/bin:$PATH>>~/.profile
+source ~/.profile
+
+#docker
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+
+# Add the repository to your APT sources
+sudo echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+
+# Then import the repository key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+# Install docker
+sudo apt-get update
+sudo apt-get install -y docker.io cgroup-lite apparmor
+
+echo Add group docker to current user
+sudo usermod -a -G docker $USER
+
+#Install AWS CLI
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+#Install EventStore
+curl -s https://packagecloud.io/install/repositories/EventStore/EventStore-OSS/script.deb.sh | sudo bash
+
+#aliases
+echo "alias gs='git status'">>~/.profile
+source ~/.profile
