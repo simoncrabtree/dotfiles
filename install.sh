a48fa0e0d9fc2e78db09ec280077d27610979afe
Simon Crabtree <simon@crabsticks.net>
2016-07-15 10:34:36 +0100

    Create symlink to Sublime settings

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
